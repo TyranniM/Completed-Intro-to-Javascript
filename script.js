afc5d7dc6b91e94ba8019c8fb544dfe8009e1365
COR Students <corstudents@Matts-MacBook-Pro-89.local>
2018-10-03 13:18:29 -0400

    completed intro-to-javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+var name = "Tyranni"; /*this is a string */
+var age = 22; /*this is an int */
+var pi = 3.14;/*this is a float / double */
+var lovesCrablegs = true; /*this is a string */
+
+/*Camel Casing*/
+var macbookColor = "silver";
+
+/* No Decimals For INTs*/
+var numberOfMAcbooks = 26;
+
+var aDecimal = 3.2 
+
+/* Bools are ONLY true of false*/
+var trueOrFalse = false
+
+/*String Concatenation*/
+var firstName = "Tyranni"
+var lastName = "Mathews"
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var twelve = 12;
+var notTwelve = false;
+var answer = twelve + notTwelve;
+
+
+/* (< > >= <= + % / * ++ --) */ /*THESE ARE MATH SYMBOLS*/
+
+var x = 2
+var y = 2.6
